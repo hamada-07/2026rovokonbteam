2a942755c0bc408ed07a904715f40c131479bb84
r07i36 <r07i36@ed.cc.suzuka-ct.ac.jp>
2026-05-20 17:39:31 +0900

    デバック出力以外の処理を割り込み関数直に書いた

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -146,30 +146,10 @@ for(int i=0;i<4;i++)motor_init(&mainMotor[i]);
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    if(!Tflag)continue;
-    Tflag = false;
-    printf("tick:%d",HAL_GetTick());
-    //sbus読み取り
-    sbus_update();
-    lf = dead(sbus_get(3), -0.1f, 0.1f);
-    ls = dead(sbus_get(4), -0.1f, 0.1f);
-    rf = dead(sbus_get(2), -0.1f, 0.1f);
-    rs = dead(sbus_get(1), -0.1f, 0.1f);
-
-    if(!sbus_get(7)){
-      if(sbus_get(6))kp=sbus_get(5)*600.0f;
-      // else ki=sbus_get(5)*600.0f;
-    }
-
-    //緊急停止
-    if(!sbus_get(9)){
-      print("stop!\n",0);
-      stop();
-      continue;
-    }
-
-    //Can出力
-    OmniControl(lf,ls,rs);
+    // if(!Tflag)continue;
+    // Tflag = false;
+    // printf("tick:%d",HAL_GetTick());
+    
     
     
     // printf("%d,",mainMotor[0].speed);
@@ -578,7 +558,31 @@ void stop(){
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
   if(htim == &htim15){
-    Tflag = true;
+    // Tflag = true;
+    //sbus読み取り
+    sbus_update();
+    lf = dead(sbus_get(3), -0.1f, 0.1f);
+    ls = dead(sbus_get(4), -0.1f, 0.1f);
+    rf = dead(sbus_get(2), -0.1f, 0.1f);
+    rs = dead(sbus_get(1), -0.1f, 0.1f);
+
+    if(!sbus_get(7)){
+      if(sbus_get(6))kp=sbus_get(5)*600.0f;
+      // else ki=sbus_get(5)*600.0f;
+    }
+
+    //緊急停止
+    if(!sbus_get(9)){
+      print("stop!\n",0);
+      stop();
+    }
+    else {
+      //Can出力
+    OmniControl(lf,ls,rs);
+    }
+    
+
+    
   }
 }
 
